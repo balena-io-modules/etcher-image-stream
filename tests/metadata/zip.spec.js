e942a97a5a2bd4d1c4d2195104d42a10287ab5c2
Juan Cruz Viotti <jviottidc@gmail.com>
2016-07-11 10:54:19 -0400

    Support a `name` property in an optional `_info/manifest.json`
    We've discussed supporting rich image extensions by allowing an optional
    `_info` directory containing a `manifest.json` file that would allow
    image publishers to include extra files with their images (like logos,
    etc).
    
    As a first step towards this goal, this PR supports a
    `_info/manifest.json` containing a `name` property.
    
    See: https://github.com/resin-io/etcher/issues/470
    Signed-off-by: Juan Cruz Viotti <jviottidc@gmail.com>

---
+++ tests/metadata/zip.spec.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016 Resin.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const m = require('mochainon');
+const path = require('path');
+const DATA_PATH = path.join(__dirname, '..', 'data');
+const IMAGES_PATH = path.join(DATA_PATH, 'images');
+const ZIP_PATH = path.join(DATA_PATH, 'metadata', 'zip');
+const tester = require('../tester');
+const imageStream = require('../../lib');
+
+describe('EtcherImageStream: Metadata ZIP', function() {
+
+  this.timeout(10000);
+
+  describe('given an archive with an invalid `manifest.json`', function() {
+
+    const archive = path.join(ZIP_PATH, 'rpi-invalid-manifest.zip');
+
+    tester.expectError(
+      path.join(ZIP_PATH, 'rpi-invalid-manifest.zip'),
+      'Invalid archive manifest.json');
+
+  });
+
+  describe('given an archive with a `manifest.json` containing a name', function() {
+
+    const archive = path.join(ZIP_PATH, 'rpi-with-name.zip');
+
+    tester.extractFromFilePath(
+      archive,
+      path.join(IMAGES_PATH, 'raspberrypi.img'));
+
+    it('should read the manifest name property', function(done) {
+      imageStream.getFromFilePath(archive).then((image) => {
+        m.chai.expect(image.name).to.equal('Raspberry Pi');
+        done();
+      });
+    });
+
+  });
+
+});
